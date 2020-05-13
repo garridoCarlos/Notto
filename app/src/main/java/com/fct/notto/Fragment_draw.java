51019c58c018995b3dc569fea20ec7324cdd104d
garridoCarlos <gscarlos.720@gmail.com>
2020-05-13 21:09:57 +0200

    Initial commit

---
+++ app/src/main/java/com/fct/notto/Fragment_draw.java
@@ -0,0 +1,25 @@
+package com.fct.notto;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment_draw extends Fragment {
+    private View view;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.draw_fragment, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/fct/notto/MainActivity.java
+++ app/src/main/java/com/fct/notto/MainActivity.java
@@ -1,14 +1,44 @@
 package com.fct.notto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    public NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    //Botones de la barra superior
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.userIcon) {
+            navController.navigate(R.id.fragment_user);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
